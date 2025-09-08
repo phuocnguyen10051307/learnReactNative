8bffb83a2731f34a335d90db6cd3bf0e2dd5135b
phuocnguyen10051307 <phuocmobi4321@gmail.com>
2025-09-08 13:03:22 +0700

    use useState increase and descrease in react native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, View,Image } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  let [check, SetCheck] = useState(false)
+  let [check, SetCheck] = useState<boolean>(false)
+  let [num, setNum] = useState<number>(0)
   let a: string = "không muốn bị scam thì chuyển khoản cho tao";
   const hanleOnclick = () => {
     SetCheck(!check)
-    Alert.alert(`${a}`)
+    num < 10 ? setNum(num + 1) : setNum(0)
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.bt1}>
-        <Button title='click' onPress={hanleOnclick} />
-      </Text>
-
+      <Text style={styles.hello1}>{`${num}`}</Text>
+      {/* <Text style={styles.bt1} onPress={hanleOnclick}>Click me 
+      </Text> */}
+      <Button title='Increase ' onPress={()=>setNum(num+1)}/>
+      <Button title='Descrease'onPress={()=>setNum(num-1)}/>
     </View>
   );
 }
@@ -31,17 +33,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   hello1: {
-    color: `red`,
+    color: `greenlight`,
     fontSize: 50,
-    height:500,
+    height: 500,
 
   },
-  bt1:{
-    width:200,
-    textAlign:`center`,
-    marginVertical:8,
-    backgroundColor:`yellow`,
-    color:'red',
-    fontSize:20,
+  bt1: {
+    width: 200,
+    textAlign: `center`,
+    marginVertical: 8,
+    backgroundColor: `yellow`,
+    color: 'red',
+    fontSize: 20,
   }
 });
