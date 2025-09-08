ab988859b442bef760ee244ad5b2e219523d5efc
phuocnguyen10051307 <phuocmobi4321@gmail.com>
2025-09-08 12:14:02 +0700

    learn use StyleSheet and test button in react native

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
+  let [check, SetCheck] = useState(false)
+  let a: string = "không muốn bị scam thì chuyển khoản cho tao";
+  const hanleOnclick = () => {
+    SetCheck(!check)
+    Alert.alert(`${a}`)
+  }
   return (
     <View style={styles.container}>
-      <Text>Lần đầu học react native 2025  </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.bt1}>
+        <Button title='click' onPress={hanleOnclick} />
+      </Text>
+
     </View>
   );
 }
 
+
+// react native không có css 
+// View = div
+// Texxt = p 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +30,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: `red`,
+    fontSize: 50,
+    height:500,
+
+  },
+  bt1:{
+    width:200,
+    textAlign:`center`,
+    marginVertical:8,
+    backgroundColor:`yellow`,
+    color:'red',
+    fontSize:20,
+  }
 });
